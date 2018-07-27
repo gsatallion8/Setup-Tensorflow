72a49e8236262d6278aeafc68b61e620a8c88e43
Srikanth Sarma <gsarma8@gmail.com>
2018-07-27 15:15:24 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install htop
+sudo apt install unzip
+sudo apt install python-pip
+sudo apt install python3-pip
+sudo pip3 install -r requirements.txt
+sudo apt-get install python3-tk
